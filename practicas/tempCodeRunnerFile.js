700fd32af9aefc96e94ee2cb524d4d7c53384484
Gadd88 <matiassaade@gmail.com>
2023-06-15 20:04:45 -0300

    ejemplos de arrays y metodos

--- practicas/tempCodeRunnerFile.js
+++ practicas/tempCodeRunnerFile.js
@@ -1,10 +1,27 @@
-const duplicados = (arr1, arr2) =>{
-    const heroesAvg = arr1.map((obj)=> obj.hero);
-    const heroesX = arr2.map((obj)=> obj.hero);
+const arrayHeroes = [
+    {name: 'Iron Man', team: ['Avengers'], age: 40, p: {a:1}},
+    {name: 'Captain America', team: ['Avengers'], age: 95},
+    {name: 'Thor', team: ['Avengers'], age: 1000},
+    {name: 'Hulk', team: ['Avengers'], age: 45},
+    {name: 'Wolverine', team: ['X.Men', 'Avengers'], age: 150},
+    {name: 'Cyclops', team: ['X-Men'], age: 35},
+    {name: 'Storm', team: ['X-Men'], age: 40},
+    {name: 'Jean Gray', team: ['X-Men'], age: 32},
+    {name: 'Black Widow', team: ['Avengers'], age: 38},
+    {name: 'Beast', team: ['X-Men'], age: 42},
+    {name: 'Rogue', team: ['X-Men'], age: 29},
+    {name: 'Scarlet Witch', team: ['Avengers', 'X-Men'], age: 33},
+    {name: 'Iron Man', team: ['Avengers'], age: 50},
+]
 
-    const heroesComun = heroesAvg.filter(hero => heroesX.includes(hero));
-    return heroesComun;
-};
+//Ordenar por edad
+// const newArray = arrayHeroes.sort((a,b) => b.age - a.age);
+// console.log(newArray)
 
-const heroesDuplis = duplicados(avengers, x_men);
-console.log(heroesDuplis)
+// //Filtrar por edad
+// const newEdad = arrayHeroes.filter(char => char.age>45)
+// console.log(newEdad)
+
+//Filtrar por Equipo
+const newTeam = arrayHeroes.includes(arrayHeroes.team == 'Avengers')
+console.log(newTeam)
