2d66d97bb9df5331ebb8bc7f8666eb4be797d837
Gadd88 <matiassaade@gmail.com>
2023-07-24 14:41:43 -0300

    initial commit, starting marvel app

---
+++ practicas/marvel-app/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <header className='page'>
+        <h1>Marvel Searcher App</h1>
+        <form action="" className="form">
+          <input 
+            type="text" 
+            placeholder='Ironman, Spiderman...' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+      <main>
+        {/* Lista de personajes */}
+
+      </main>
+    </>
+  )
+}
+
+export default App
