b310767b67de0aed837aa8ef305e2093244aa310
Gadd88 <matiassaade@gmail.com>
2023-07-24 15:29:42 -0300

    testing the api response

--- practicas/marvel-app/src/App.jsx
+++ practicas/marvel-app/src/App.jsx
@@ -1,9 +1,20 @@
-
+import { useState, useEffect } from 'react';
 import './App.css'
+//import apiResponse from './mocks/apiResponse.json'
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=6bff9b32ac69d54beea1d9ba1e17f5af&hash=bd52789632bd7b951b88f9bbf58004ef')
+      .then(res => res.json())
+      .then(data => setCharacters(data.data.results))
+  }, [])
   
 
+  //|const characters = apiResponse.data.results
+  // const hasChars = characters?.length > 0  
+  
   return (
     <>
       <header className='page'>
@@ -17,7 +28,26 @@ function App() {
       </header>
       <main>
         {/* Lista de personajes */}
-
+        {
+          characters ?  
+          (
+              <ul>
+                {
+                  characters.map((char) => {
+                    return (
+                      <li key={char.id}>
+                        <img src={`${char.thumbnail.path}.${char.thumbnail.extension}`} alt="" />
+                        <h3>{char.name}</h3>
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            )  
+          : (
+              <p>No se encuentran resultados</p>
+            )
+        }
       </main>
     </>
   )
